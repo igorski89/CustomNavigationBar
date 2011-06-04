d4621288aadac2141a9cb7216530d46ba6671627
Igor Evsukov <igor.evsukov@gmail.com>
2011-06-04 19:06:20 +0300

    initial commit

---
+++ Sources/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  CustomNavigationBarAppDelegate.h
+//  CustomNavigationBar
+//
+//  Created by Igor Evsukov on 04.06.11.
+//  Copyright 2011 Igor Evsukov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CustomNavigationBar
+//
+//  Created by Igor Evsukov on 04.06.11.
+//  Copyright 2011 Igor Evsukov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
